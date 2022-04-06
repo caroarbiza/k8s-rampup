a941d3c2e6ecad28f0fb12b48ed7300a4eddfd99
caroarbiza <carolina.arbiza@endava.com>
2022-04-06 19:57:39 -0300

    update

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 
-kubectl apply -f backend/service.yml
+kubectl apply -f /home/ubuntu/k8s-rampup/backend/service.yml
 
-kubectl apply -f backend/deploy.yml
+kubectl apply -f /home/ubuntu/k8s-rampup/backend/deployment.yml
 
-kubectl apply -f frontend/service.yml
+kubectl apply -f /home/ubuntu/k8s-rampup/frontend/service.yml
 
-kubectl apply -f frontend/deploy.yml
+kubectl apply -f /home/ubuntu/k8s-rampup/frontend/deployment.yml
 
-kubectl create -f frontend/ingress.yml
+kubectl create -f /home/ubuntu/k8s-rampup/frontend/ingress.yml
